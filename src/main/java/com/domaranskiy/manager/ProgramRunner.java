84b5c966eda6892af5eae89783fa942a12ba8ce1
Sergiy D <SergiyDom@users.noreply.github.com>
2020-07-20 06:56:26 +0300

    done chain_of_responsibility pattern

--- src/main/java/com/domaranskiy/manager/ProgramRunner.java
+++ src/main/java/com/domaranskiy/manager/ProgramRunner.java
@@ -1,6 +1,7 @@
 package com.domaranskiy.manager;
 
 import com.domaranskiy.builder.*;
+import com.domaranskiy.chain_of_responsibility.*;
 import com.domaranskiy.singleton.ProgramLogger;
 
 public class ProgramRunner {
@@ -14,10 +15,23 @@ public class ProgramRunner {
         System.out.println("hash code at the end - " + ProgramLogger.getProgramLogger().toString() + "\n");
     }
 
-    public static void builderRunner(){
+    public static void builderRunner() {
         Director director = new Director();
         director.setBuilder(new TenagerCarBuilder());
         Car car = director.buildCar();
-        System.out.println(car);
+        System.out.println(car + "\n");
+    }
+
+    public static void bugTracker() {
+        Notifier reporrtNotifier = new SimpleReportNotifier(Priority.ROUTINE);
+        Notifier emailNotifier = new EmailNotifier(Priority.IMPORTANT);
+        Notifier smsNotifier = new SMSnotifier(Priority.ASAP);
+
+        reporrtNotifier.setNextNotifier(emailNotifier);
+        emailNotifier.setNextNotifier(smsNotifier);
+
+        reporrtNotifier.notifyManager("Everything is OK", Priority.ROUTINE);
+        reporrtNotifier.notifyManager("Something went wrong!", Priority.IMPORTANT);
+        reporrtNotifier.notifyManager("We've had a problem here!!!", Priority.ASAP);
     }
 }
